79667ec8896acb470d479369a1531c299be25398
Prakhar Sethi <prakharsethi05@gmail.com>
2026-02-26 22:02:54 +0530

    Changes

--- server/models/User.js
+++ server/models/User.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email:    { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true, minlength: 6 },
-  avatar:   { type: String, default: '' },
+  username:   { type: String, required: true, unique: true, trim: true },
+  email:      { type: String, required: true, unique: true, lowercase: true },
+  password:   { type: String, required: true, minlength: 6 },
+  avatar:     { type: String, default: '' },
   likedSongs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Song' }],
   playlists:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Playlist' }],
 }, { timestamps: true });
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(12);
+  this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
